4a2e9388f432b96d6e32151be3916d2e9549f686
clmntpllr <25619661+clmntpllr@users.noreply.github.com>
2022-04-20 22:10:31 +0200

    Simplification navbar, ajout lien dashboard bourse

--- bourse/context_processors.py
+++ bourse/context_processors.py
@@ -1,4 +1,9 @@
 from django.conf import settings
+from .models import Event
 
 def site_params(request):
     return {'SITE_TITLE_SETTING': settings.SITE_TITLE, 'SITE_SIGNATURE_SETTING': settings.SITE_SIGNATURE}
+
+def active_event(request):
+    active_event = Event.objects.filter(status__in=[1,2,3])
+    return {'active_event':active_event}
