1199a2984d2c4050f4524ce98f8b12fee16b751b
clmntpllr <25619661+clmntpllr@users.noreply.github.com>
2022-07-05 18:34:41 +0200

    nettoyage code

--- woopmart/urls.py
+++ woopmart/urls.py
@@ -16,27 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.views.generic import RedirectView
 from django.urls import include,path
-#from django.contrib.auth import views
-#from django.conf import settings
-#from django.conf.urls.static import static
 from django_registration.forms import RegistrationFormUniqueEmail
 from django_registration.backends.activation.views import RegistrationView
-#from django_registration.views import RegistrationView
 from .forms import CustomRegistrationForm
 
 urlpatterns = [
     path('',RedirectView.as_view(url='bourse/',permanent=True)), # Redirection vers bourse/
     path('admin/', admin.site.urls),
     path('bourse/', include('bourse.urls')),
-    #path('accounts/register/',RegistrationView.as_view(form_class=RegistrationFormUniqueEmail),name='django_registration_register'),
     path('accounts/register/',RegistrationView.as_view(form_class=CustomRegistrationForm),name='django_registration_register'),
     path('accounts/', include('django_registration.backends.activation.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-    # path('accounts/login', views.LoginView.as_view(), name='login'),
-    # path('accounts/logout/', views.LogoutView.as_view(), name='logout'),
 ]
 
-# Use static() to add url mapping to serve static files during development (only)
-#urlpatterns += [
-#    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
-#]
